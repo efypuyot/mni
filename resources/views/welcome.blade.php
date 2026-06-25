52ac602cacd8273cda01e5aac8747075672a0a0c
ellen <eypuyot@up.edu.ph>
2026-06-25 23:03:45 +0800

    [mni#3] update app name]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel React App</title>
+    <title>My name is...? <by efypuyot/></title>
 
     @viteReactRefresh
     
